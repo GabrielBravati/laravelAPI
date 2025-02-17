262ef457dc745e5ed651ba0bb6f74653aad95bb0
Gabriel Bravati <gabrielbravati@gmail.com>
2025-02-17 12:37:19 -0300

    FIX: Álbuns Faixas

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -5,10 +5,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Faixa;
 class Album extends Model
 {
     use HasFactory;
+    protected $table = 'albuns';
 
     protected $fillable = [
         'nome',
